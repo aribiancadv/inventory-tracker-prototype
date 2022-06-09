c4109c78d0a8711b2971089817fc0da10bec8bb4
Ari <ari.di-veroli.2021@uni.strath.ac.uk>
2022-06-09 17:25:04 +0100

    tidied up the code in ItemsController.java

--- src/main/java/com/javatpoint/controller/ItemsController.java
+++ src/main/java/com/javatpoint/controller/ItemsController.java
@@ -14,39 +14,43 @@ import com.javatpoint.service.ItemsService;
 @RestController
 public class ItemsController 
 {
-//autowire the ItemsService class
-@Autowired
-ItemsService itemsService;
-//creating a get mapping that retrieves all the items detail from the database 
-@GetMapping("/items")
-private List<InvItems> getAllItems() 
-{
-return itemsService.getAllItems();
-}
-//creating a get mapping that retrieves the detail of a specific item
-@GetMapping("/items/{itemid}")
-private InvItems getItems(@PathVariable("itemid") int itemid) 
-{
-return itemsService.getItemsById(itemid);
-}
-//creating a delete mapping that deletes a specified item
-@DeleteMapping("/items/{itemid}")
-private void deleteItem(@PathVariable("itemid") int itemid) 
-{
-itemsService.delete(itemid);
-}
-//creating post mapping that post the item detail in the database
-@PostMapping("/items")
-private int saveItem(@RequestBody InvItems items) 
-{
-itemsService.saveOrUpdate(items);
-return items.getItemid();
-}
-//creating put mapping that updates the item detail 
-@PutMapping("/items")
-private InvItems update(@RequestBody InvItems items) 
-{
-itemsService.saveOrUpdate(items);
-return items;
-}
+    //autowire the ItemsService class
+    @Autowired
+    ItemsService itemsService;
+    //creating a get mapping that retrieves all the items detail from the database 
+    @GetMapping("/items")
+    private List<InvItems> getAllItems() 
+    {
+        return itemsService.getAllItems();
+    }
+
+    //creating a get mapping that retrieves the detail of a specific item
+    @GetMapping("/items/{itemid}")
+    private InvItems getItems(@PathVariable("itemid") int itemid) 
+    {
+        return itemsService.getItemsById(itemid);
+    }
+
+    //creating a delete mapping that deletes a specified item
+    @DeleteMapping("/items/{itemid}")
+    private void deleteItem(@PathVariable("itemid") int itemid) 
+    {
+        itemsService.delete(itemid);
+    }
+
+    //creating post mapping that post the item detail in the database
+    @PostMapping("/items")
+    private int saveItem(@RequestBody InvItems items) 
+    {
+        itemsService.saveOrUpdate(items);
+        return items.getItemid();
+    }
+
+    //creating put mapping that updates the item detail 
+    @PutMapping("/items")
+    private InvItems update(@RequestBody InvItems items) 
+    {
+        itemsService.saveOrUpdate(items);
+        return items;
+    }
 }
